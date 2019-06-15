4b64288303435506823e3f6488781fb434a7470a
Devin Davis <DLC@devin-daviss-macbook-pro-729.local>
2019-06-15 14:06:36 -0400

    updating mobile view

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Row, Col } from 'react-materialize';
 
 class Home extends React.Component {
     render () {
@@ -7,9 +8,10 @@ class Home extends React.Component {
         <br></br>
         {/* <h1 className="container">Devin Davis</h1> */}
         <br></br>
+        <Col s={12}>
         <h2 className="intro container white-text"><b>HEY THERE, <br></br>I AM DEVIN DAVIS</b></h2>
         <h5 className="blue darken-1  white-text">Product Manager | Software Engineer</h5>
-        
+        </Col>
             </div>
       );
     }
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -17,28 +17,28 @@ class Portfolio extends React.Component {
 
 <div className='blue darken-2'>
 <a target="_blank" href='https://kicks-stock.herokuapp.com/kicks/user'> 
-<h2 className=" btn blue white-text">Kicks Stock</h2></a>
+<h3 className=" btn blue white-text">Kicks Stock</h3></a>
 <p className='white-text'>Kicks Stock is a sneaker app</p>
 <img src='https://imgur.com/7gyDsYA.png'/>
 </div> 
 
 <div className='green darken-1'>
 <a target="_blank" href='https://jobshunter.herokuapp.com/'> 
-<h2 className=" btn green white-text">JobHunters</h2></a>
+<h3 className=" btn green white-text">JobHunters</h3></a>
 <p className='white-text'>JobHunters is a app that tracks and adds potenial jobs</p>
 <img src='https://imgur.com/nfvZj8I.png'/>
 </div> 
 
 <div className='red darken-1'>
 <a target="_blank" href='https://tonus-moods-eq.herokuapp.com/'> 
-<h2 className=" btn red white-text">Tonus Mood EQ</h2></a>
+<h3 className=" btn red white-text">Tonus Mood EQ</h3></a>
 <p className='white-text'>Tonus Mood EQ is a ... app</p>
 <img src='https://imgur.com/gqQ64WQ.png'/>
 </div> 
 
 <div className='grey darken-4'>
 <a target="_blank" href='http://tonusaudio.com/'> 
-<h2 className=" btn grey darken-3 red white-text">Tonus Audio</h2></a>
+<h3 className=" btn grey darken-3 red white-text">Tonus Audio</h3></a>
 <p className='white-text'>Tonus Mood EQ is a ... app</p>
 <img src='https://imgur.com/XsqWpEz.png'/>
 </div> 
