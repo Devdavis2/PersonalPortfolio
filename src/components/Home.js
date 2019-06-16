2b8cd6b5e72bbbd3b5bb05ee79ef6579f28bbba8
Devin Davis <DLC@devin-daviss-macbook-pro-729.local>
2019-06-15 22:25:12 -0400

    prevent wrapping on mobile

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,9 +8,9 @@ class Home extends React.Component {
         <br></br>
         {/* <h1 className="container">Devin Davis</h1> */}
         <br></br>
-        <Col s={12}>
+        <Col s={11}>
         <h2 className="intro container white-text"><b>HEY THERE, <br></br>I AM DEVIN DAVIS</b></h2>
-        <h5 className="sub_intro blue darken-1  white-text">Product Manager | Software Engineer</h5>
+        <h5 className="sub_intro blue darken-1 flow-text white-text">Product Manager | Software Engineer</h5>
         </Col>
             </div>
       );
