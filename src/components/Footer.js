37b8f24836d90218bdf2af11977fcbf44b6ef0bc
Devin Davis <DLC@devin-daviss-macbook-pro-729.local>
2019-06-18 15:14:01 -0400

    updated styling footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,16 +6,18 @@ class Footer extends React.Component {
         <footer class="page-footer grey lighten-3 black-text">
           <div class="container">
             <div class="row">
-              <div class="col l6 s12">
-                <h5 class="blue-text">Footer Content</h5>
-                <p class="black-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
-              </div>
-              <div class="col l4 offset-l2 s12">
-                <h5 class="blue-text">Links</h5>
+              <div class="col l6 s6">
+                <h5 class="blue-text">Follow Me</h5>
+                {/* <p class="black-text text-lighten-4">You can use rows and columns here to organize your footer content.</p> */}
                 <ul>
-                  <li><a class="black-text text-lighten-3" href="/Resume">Resume</a></li>
                   <li><a class="black-text text-lighten-3" href="http://twitter.com">Twitter</a></li>
                   <li><a class="black-text text-lighten-3" href="http://instagram.com">Instagram</a></li>
+                </ul>
+              </div>
+              <div class="col l4 offset-l2 s6">
+                <h5 class="blue-text">Experience</h5>
+                <ul>
+                  <li><a class="black-text text-lighten-3" href="/Resume">Resume</a></li>
                 </ul>
               </div>
             </div>
