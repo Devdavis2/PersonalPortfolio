cd3aaba6e4434ef6295bee3a87858f2274e750d6
Devin Davis <DLC@devin-daviss-macbook-pro-729.local>
2019-06-20 14:18:43 -0400

    updating header

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,7 +10,7 @@ class Home extends React.Component {
         <br></br>
         {/* <Col s={10}> */}
         <h3 className="intro container white-text"><b>DEVIN DAVIS,</b></h3>
-        <h3 className="title container white-text"><b>CREATIVE TECHNOLOGIST</b></h3>
+        <h4 className="title container white-text"><b>CREATIVE TECHNOLOGIST</b></h4>
         <h5 className="sub_intro blue darken-1 flow-text white-text">Product Manager | Software Engineer</h5>
         {/* </Col> */}
             </div>
