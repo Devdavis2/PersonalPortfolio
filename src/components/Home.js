2fedd7c17bab04d739f62a7266dd5630b74508e5
Devin Davis <DLC@devin-daviss-macbook-pro-729.local>
2019-06-23 12:56:26 -0400

    updating styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,7 +11,7 @@ class Home extends React.Component {
         {/* <Col s={10}> */}
         <h4 className="intro container white-text"><b>CREATIVE</b></h4>
         <h4 className="title container white-text"><b>TECHNOLOGIST</b></h4>
-        <h5 className="sub_intro blue darken-1 flow-text white-text">Product Manager | Software Engineer</h5>
+        <h5 className="sub_intro blue flow-text white-text">Product Manager | Software Engineer</h5>
         {/* </Col> */}
             </div>
       );
