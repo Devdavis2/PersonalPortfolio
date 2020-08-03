8ca8a0611cdbb9dc4dfa22d589bcb9c000f42416
Devdavis2 <dev.davis2@gmail.com>
2020-08-02 23:25:52 -0400

    Testing Sub-headline

--- src/components/Home.js
+++ src/components/Home.js
@@ -24,9 +24,9 @@ class Home extends React.Component {
           <h4 className="intro container white-text">
             <b>MarTech</b>
           </h4>
-          <h4 className="title container white-text">
+          <h6 className="title flow-text container white-text">
             <b>Management Specialist</b>
-          </h4>
+          </h6>
         </div>
       </div>
     );
