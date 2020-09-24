9b63f0b1fe950fa5945a80be92a2124533329453
Devdavis2 <dev.davis2@gmail.com>
2020-09-24 09:05:34 -0400

    testing redirect on resume page

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,24 +1,40 @@
-import React from 'react'
-import ResumePDF from './images/Resume.pdf'
+import React from "react";
+import { Redirect } from "react-router-dom";
+import ResumePDF from "./images/Resume.pdf";
 
 class Resume extends React.Component {
-    render() {
-        return (
-            <div className='resume'>
-                <object
-                    data={ResumePDF}
-                    type='applications/pdf'
-                    width='100%'
-                    height='100vh'>
-                    <iframe
-                        src={ResumePDF}
-                        width='100%'
-                        style={{ height: '100vh' }}>
-                    </iframe>
-                </object>
-            </div>
-        );
+  state = {
+    redirect: false,
+  };
+  setRedirect = () => {
+    this.setState({
+      redirect: true,
+    });
+  };
+  renderRedirect = () => {
+    if (this.state.redirect) {
+      return <Redirect to="/Resume" />;
     }
+  };
+
+  render() {
+    return (
+      <div className="resume">
+        <object
+          data={ResumePDF}
+          type="applications/pdf"
+          width="100%"
+          height="100vh"
+        >
+          <iframe
+            src={ResumePDF}
+            width="100%"
+            style={{ height: "100vh" }}
+          ></iframe>
+        </object>
+      </div>
+    );
+  }
 }
 
 export default Resume;
